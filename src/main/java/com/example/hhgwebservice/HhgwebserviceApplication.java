d2514af1f7e274e3138f7304ff345b6af7a10cb2
Heo <gjgudrn1114@naver.com>
2023-07-04 16:12:09 +0900

    프로젝트환경구축

---
+++ src/main/java/com/example/hhgwebservice/HhgwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhgwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhgwebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhgwebserviceApplication.class, args);
+    }
+
+}
