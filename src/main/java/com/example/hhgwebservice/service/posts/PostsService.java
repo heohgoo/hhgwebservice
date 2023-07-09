02b5c88e569898e528e970e4a10009afbf1bebaa
Heo <gjgudrn1114@naver.com>
2023-07-09 13:33:20 +0900

    수정/조회API제작및테스트

--- src/main/java/com/example/hhgwebservice/service/posts/PostsService.java
+++ src/main/java/com/example/hhgwebservice/service/posts/PostsService.java
@@ -1,13 +1,16 @@
 package com.example.hhgwebservice.service.posts;
 
+import com.example.hhgwebservice.domain.posts.Posts;
 import com.example.hhgwebservice.domain.posts.PostsRepository;
+import com.example.hhgwebservice.web.dto.PostsResponseDto;
 import com.example.hhgwebservice.web.dto.PostsSaveRequestDto;
+import com.example.hhgwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor //final, not null
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
@@ -15,5 +18,23 @@ public class PostsService {
     @Transactional //트랜잭션 - 모든 작업들이 성공해야만 최종적으로 데이터베이스에 반영되도록 한다.
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id ="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        //repository로 쿼리를 날리지 않는다. => JPA의 영속성 컨텍스트(엔티티를 영구 저장하는 환경)
+        //entity 객체의 값만 변경하면 별도로 update 쿼리를 날릴 피료가 없게 된다.(더티 체킹)
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
