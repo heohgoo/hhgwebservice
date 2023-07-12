dc5432276ab5ebd171acd4ebeeadb30198dcf3b5
Heo <gjgudrn1114@naver.com>
2023-07-12 16:03:05 +0900

    게시글갱신/삭제기능추가

--- src/main/java/com/example/hhgwebservice/web/PostsApiController.java
+++ src/main/java/com/example/hhgwebservice/web/PostsApiController.java
@@ -5,12 +5,18 @@ import com.example.hhgwebservice.web.dto.PostsResponseDto;
 import com.example.hhgwebservice.web.dto.PostsSaveRequestDto;
 import com.example.hhgwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
@@ -22,9 +28,10 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById (@PathVariable Long id) {
-        return postsService.findById(id);
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long postsDelete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 
 }
