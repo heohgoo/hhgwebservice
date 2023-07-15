b1c3bccf2f41e702016e286d4a06be50a1b47374
Heo <gjgudrn1114@naver.com>
2023-07-15 21:21:46 +0900

    구글로그인/로그아웃/권한관리

--- src/main/java/com/example/hhgwebservice/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/hhgwebservice/config/auth/dto/SessionUser.java
@@ -1,11 +1,13 @@
 package com.example.hhgwebservice.config.auth.dto;
 
 import com.example.hhgwebservice.domain.user.User;
+import lombok.Getter;
 
 import java.io.Serializable;
 
 //인증된 사용자 정보만을 필요로 한다.
 //직렬화 기능을 가진 Dto를 통해 성능 이슈, 부수 효과 발생을 줄인다.(User 클래스에 직렬화를 구현하지 않음을 통해)
+@Getter
 public class SessionUser implements Serializable {
     private String name;
     private String email;
--- src/main/java/com/example/hhgwebservice/web/IndexController.java
+++ src/main/java/com/example/hhgwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.hhgwebservice.web;
 
+import com.example.hhgwebservice.config.auth.dto.SessionUser;
 import com.example.hhgwebservice.service.posts.PostsService;
 import com.example.hhgwebservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,23 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
