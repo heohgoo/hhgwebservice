4e5ad7b2e28e0abbbbf502824d1a219e09dd4038
Heo <gjgudrn1114@naver.com>
2023-07-21 15:11:30 +0900

    EC2배포/RDS 연동

--- src/main/java/com/example/hhgwebservice/HhgwebserviceApplication.java
+++ src/main/java/com/example/hhgwebservice/HhgwebserviceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//AWS EC2에서 Linux기반 AMI로 배포 예정
-//MariaDB로 AWS RDS 연동 예정
-
+//AWS EC2에서 Linux기반 AMI로 배포 완료
+//MariaDB로 AWS RDS 연동 완료
+//테스트 환경 구성
 
 
 //@EnableJpaAuditing => 테스트 에러로 분리
