1abf84dd8cdd0161cec8c633972cf2ea773f0450
Heo <gjgudrn1114@naver.com>
2023-07-21 13:47:21 +0900

    서버배포준비

--- src/main/java/com/example/hhgwebservice/HhgwebserviceApplication.java
+++ src/main/java/com/example/hhgwebservice/HhgwebserviceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//AWS EC2에서 Linux기반 AMI로 배포 예정
+//MariaDB로 AWS RDS 연동 예정
+
+
+
 //@EnableJpaAuditing => 테스트 에러로 분리
 // JPA Auditing 가능하게끔
 @SpringBootApplication
